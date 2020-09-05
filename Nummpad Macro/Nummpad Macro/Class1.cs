8d65c890f0169bab6d00e03ac1bddead756b68c1
daniel-code007 <69769229+daniel-code007@users.noreply.github.com>
2020-09-05 17:28:26 +0200

    Add files via upload

---
+++ Nummpad Macro/Nummpad Macro/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nummpad_Macro
+{
+    class Keylogger
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+
+
+         static void Main(string[] args)
+        {
+            while(true)
+            {
+                Thread.Sleep(5);
+                for (int i = 1; i < 130; i++ )
+                {
+                    int KeyState = GetAsyncKeyState(i);
+
+                    if (KeyState == -32767)
+                    {
+                       Console.Write((char)i + ", ");
+                    }
+                   
+                }
+            }
+        }
+    }
+}
---
+++ Nummpad Macro/Nummpad Macro/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Nummpad_Macro
+{
+    public partial class NumMacPad : Form
+    {
+        public NumMacPad()
+        {
+            InitializeComponent();
+        }  
+
+        private void NumPad1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad1:
+                    Macro1.PerformClick();
+                    break;     
+
+            }
+        }
+
+        private void NumPad2(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad2:
+                    Macro2.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad3(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad3:
+                    Macro3.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad4(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad4:
+                    Macro4.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad5(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad5:
+                    Macro5.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad6(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad6:
+                    Macro6.PerformClick();
+                    break;
+
+            }
+
+        }
+
+
+        private void NumPad7(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad7:
+                    Macro7.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad8(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad8:
+                    Macro8.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void NumPad9(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad9:
+                    Macro9.PerformClick();
+                    break;
+
+            }
+        }
+
+        private void Recorder_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
